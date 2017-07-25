96aa0d899bbd5f9dac87e94e034228022d95d825
Weslley <weslleyocara@hotmail.com>
2017-07-25 18:56:58 -0300

    Finalzando a tela de vendas

--- app/views/admin/sales.py
+++ app/views/admin/sales.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request,'admin/products/index.html')
+	return render(request,'admin/sales/index.html',{'range': range(10)})
 
 def show(request,id):
-	return render(request,'admin/products/show.html')
+	return render(request,'admin/sales/show.html')
