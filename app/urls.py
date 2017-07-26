95c578fe8926aa257a15ea37bf1f64c8f2dbdcf7
Philippe <philip_will15@hotmail.com>
2017-07-26 15:08:36 -0300

    formatação PEP8 em urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from app.views import clients
 
@@ -14,27 +14,32 @@ from app.views.admin.reports import stock as reports_stock
 
 
 urlpatterns = [
-	url(r'^$', clients.login, name='home'),
+    url(r'^$', clients.login, name='home'),
     url(r'^clients/login$', clients.login, name='login'),
     url(r'^clients/register$', clients.register, name='register'),
 
-	#ADMIN
+    # ADMIN
     url(r'^admin/$', admin.index, name='admin_path'),
 
-    #PRODUCTS
+    # PRODUCTS
     url(r'^admin/products$', products.index, name='products_path'),
-    #url(r'^admin/products/(?P<id>\d+)$', products.show, name='show_product_path'),
-    
-    #SALES
+    # url(r'^admin/products/(?P<id>\d+)$',
+    #   products.show, name='show_product_path'),
+
+    # SALES
     url(r'^admin/sales$', sales.index, name='sales_path'),
-    #url(r'^admin/sales/(?P<id>\d+)$', sales.show, name='show_sales_path'),
-    
-    #ACCOUNT
+    # url(r'^admin/sales/(?P<id>\d+)$', sales.show, name='show_sales_path'),
+
+    # ACCOUNT
     url(r'^admin/account$', account.index, name='account_path'),
-    
-    #REPORTS
-    url(r'^admin/reports$', reports.index, name='reports_path'),
-    url(r'^admin/reports/products$', reports_products.index, name='reports_products_path'),
-    url(r'^admin/reports/sales$', reports_sales.index, name='reports_sales_path'),
-    url(r'^admin/reports/stock$', reports_stock.index, name='reports_stock_path')
+
+    # REPORTS
+    url(r'^admin/reports$',
+        reports.index, name='reports_path'),
+    url(r'^admin/reports/products$',
+        reports_products.index, name='reports_products_path'),
+    url(r'^admin/reports/sales$',
+        reports_sales.index, name='reports_sales_path'),
+    url(r'^admin/reports/stock$',
+        reports_stock.index, name='reports_stock_path')
 ]
