bc1d617bd1c8b78cd7b120fd89262ec0e90eed04
caiovictormc <caiovictormc@gmail.com>
2018-11-07 11:55:01 -0300

    Tratamento para valores em Real

--- app/templatetags/helper.py
+++ app/templatetags/helper.py
@@ -2,10 +2,15 @@ from django import template
 from django.core.serializers import serialize
 from django.db.models.query import QuerySet
 from django.utils.safestring import mark_safe
+
 from app.utils import api_to_date
+
 import json
+import locale
+
 
 register = template.Library()
+locale.setlocale(locale.LC_MONETARY, 'pt_BR.UTF-8')
 
 
 @register.filter(name='date_api')
@@ -15,6 +20,10 @@ def date_api(date_str):
 @register.filter(name='fmt_date')
 def fmt_date(date_str):
     return api_to_date(date_str).strftime('%d %b %Y %H:%M')
+
+@register.filter(name='fmt_money')
+def fmt_money(money_str):
+    return locale.currency(float(money_str), grouping=True)
 
 @register.filter(name='jsonify')
 def jsonify(object):
